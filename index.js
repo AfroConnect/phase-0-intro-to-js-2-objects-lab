6557679d5060ccde0bc3bd7d7f4483ff9a548704
AfroConnect <mensahmedia23@gmail.com>
2023-03-26 06:07:35 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Theophilus Mensah',
+    streetAddress: 'Robson Street, Vancouver, British Columbia'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
